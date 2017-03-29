e3ba48ff8810d9271aeebc4cf475e64b199679d2
Salsera25 <sjoblomka@gmail.com>
2017-03-28 20:39:14 -0400

    updated prev next buttons

--- scripts/album.js
+++ scripts/album.js
@@ -41,7 +41,7 @@ var createSongRow = function(songNumber, songName, songLength) {
  var clickHandler = function() {
           
 	var songNumber = parseInt($(this).attr('data-song-number'));
-	if (currentlyPlayingSongNumber!== null) {
+	if (currentlyPlayingSongNumber !== null) {
 		// Revert to song number for currently playing song because user started playing new song.
 		var currentlyPlayingCell = getSongNumberCell(currentlyPlayingCell);
 		currentlyPlayingCell.html(currentlyPlayingSongNumber);
@@ -50,13 +50,12 @@ var createSongRow = function(songNumber, songName, songLength) {
 		// Switch from Play -> Pause button to indicate new song is playing.
 		$(this).html(pauseButtonTemplate);
 		setSong(songNumber);
-              updatePlayerBarSong();
+        updatePlayerBarSong();
 	} else if (currentlyPlayingSongNumber === songNumber) {
 		// Switch from Pause -> Play button to pause currently playing song.
 		$(this).html(playButtonTemplate);
          $('.main-controls .play-pause').html(playerBarPlayButton);
-		currentlyPlayingSongNumber = null;
-        currentSongFromAlbum = null;
+		setSong(null);
 	}
 };
  
